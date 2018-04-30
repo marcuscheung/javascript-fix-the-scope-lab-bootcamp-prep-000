39aba9ad35b1f503868560902e6902bae8b24601
Marcus Cheung <myc2131@tc.columbia.edu>
2018-04-30 09:47:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,16 +12,12 @@ function add2(n) {
   return n + parseInt('2', 10)
 }
 
-var funkyFunction = function(){
-  return funkyFunction
+
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
 }
-
-function theFunk(){
-  return "FUNKY!"
 }
-
-
-
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
